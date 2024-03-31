dcfcbd6091896f03cfd1a1d7fce682949379a2d6
Arsh khan <160357899+arshkhan710@users.noreply.github.com>
2024-03-31 19:41:44 +0500

    Add files via upload

---
+++ Exercise 14-guest list/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// 14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to
+// invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+//# Create a list of guests
+// Exercise 14: Guest List
+// Create a list of guests
+let guests = ["Atiq", "Aadil", "Azhar"];
+// Print invitation messages to each person on the list
+for (let guest of guests) {
+    console.log(`Dear ${guest}, you are invited to dinner.`);
+}
