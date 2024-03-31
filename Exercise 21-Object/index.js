11b6d91c8b09297c466b318262b04ec0a75471c8
Arsh khan <160357899+arshkhan710@users.noreply.github.com>
2024-04-01 02:19:22 +0500

    Add files via upload

---
+++ Exercise 21-Object/index.js
@@ -0,0 +1,15 @@
+// 21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+var car = {
+    make: "Toyota",
+    model: 2022,
+    seats: 4,
+    color: "Blue",
+    mileage: 50000,
+    price: 100000
+};
+console.log(car);
+console.log("Make:", car.make);
+console.log("Color", car.color);
+console.log("Milage", car.mileage);
+console.log("Model:", car.model);
+console.log("Seats:", car.seats);
